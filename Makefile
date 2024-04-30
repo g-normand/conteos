3cc6b848688243c6206d255beadb291a9b6b8768
Guillaume Normand <guillaumenormand@protonmail.com>
2024-04-30 18:00:18 -0500

    Add tag

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ PROJECT_PATH := $(CURDIR)
 APP_PATH := $(CURDIR)/conteos
 ENV_PATH := $(CURDIR)/conteos_env
 PYTHON := $(ENV_PATH)/bin/python3.9
+TAG_NAME := DEPLOY
+TAG_DATE := $(TAG_NAME)_$(shell date -u "+%Y_%m_%d_%H_%M_%S")
 
 ### Shell configure. ###
 # For shell to bash to be able to use source.
@@ -32,6 +34,8 @@ shell:
 deploy: clean
 	rsync -r conteos/* --exclude conteos/staticfiles/ conteosec@ssh-conteosec.alwaysdata.net:/home/conteosec/www/conteos_db/conteos/
 	rsync Makefile conteosec@ssh-conteosec.alwaysdata.net:/home/conteosec/www/conteos_db/
+	git tag $(TAG_DATE); git push origin $(TAG_DATE) --no-verify
+	echo "Now please restart on AlwaysData"
 
 collectstatic:
 	$(VENV) && $(PYTHON) $(APP_PATH)/manage.py collectstatic
--- conteos/sites/models.py
+++ conteos/sites/models.py
@@ -8,11 +8,13 @@ class CountSite(models.Model):
     start_date = models.DateTimeField("start date")
     stop_date = models.DateTimeField("stop date")
     city = models.CharField(max_length=100)
-    latitude = models.FloatField(blank=True, null=True)
-    longitude = models.FloatField(blank=True, null=True)
-    phone_number = models.CharField(max_length=100, null=True, blank=True)
+    latitude = models.FloatField(blank=True, null=True, help_text='Between -5 and 5 usually')
+    longitude = models.FloatField(blank=True, null=True, help_text='Between -75 and -85 usually')
+    phone_number = models.CharField(max_length=10, null=True, blank=True,
+                                    help_text='One number, Ecuadorian phones only')
     email = models.CharField(max_length=100, null=True, blank=True)
-    link_url = models.CharField(max_length=100, blank=True, null=True)
+    link_url = models.CharField(max_length=100, blank=True, null=True,
+                                help_text='Link for an image (you can use https://postimages.org)')
     ebird_trip_report_url = models.CharField(max_length=100, blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
